1f565358e290e68191b5eca502998d3f911b200c
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-04-27 22:51:30 -0300

    feat: created page and route do add a new admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,8 +6,20 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
+
   def edit
-    set_admin
   end
 
   def update
