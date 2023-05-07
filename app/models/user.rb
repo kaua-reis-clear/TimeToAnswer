5b8b3cb26b150ac76d5230bdf23893f9b362ecde
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-05-07 13:24:27 -0300

    refactor: using rails validations on edit user profile form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  # Validations
+  validates :first_name, presence: true, length: { minimum: 3 }, on: :update
 
+  # Virtual Attributes
   def full_name
     [self.first_name, self.last_name].join(' ')
   end
